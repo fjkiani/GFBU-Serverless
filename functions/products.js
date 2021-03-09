8e7b65f79c9053f6b07379977c28e99c22a2837c
fahad <fjkiani@icloud.com>
2021-03-09 04:17:03 -0500

    commit

--- functions/products.js
+++ functions/products.js
@@ -44,6 +44,9 @@ exports.handler = async (event, context, cb) => {
       
     })
     return {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
       statusCode: 200,
       body: JSON.stringify(takeout),
     
